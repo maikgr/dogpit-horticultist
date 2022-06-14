b308096f7714a5a54e6e80700e98c5b46dbe4f91
maikgr <maikgibranr@gmail.com>
2022-06-14 19:46:07 +0800

    Add basic movement for NPC in town plaza

--- Assets/Scripts/Mechanics/NpcController.cs
+++ Assets/Scripts/Mechanics/NpcController.cs
@@ -5,6 +5,7 @@ namespace Horticultist.Scripts.Mechanics
     using UnityEngine;
     using TMPro;
     using Horticultist.Scripts.Core;
+    using DG.Tweening;
 
     public class NpcController : MonoBehaviour
     {
@@ -14,11 +15,19 @@ namespace Horticultist.Scripts.Mechanics
         [SerializeField] private SpriteRenderer headgearSprite;
         [SerializeField] private SpriteRenderer eyesSprite;
         [SerializeField] private SpriteRenderer mouthSprite;
+
+        [Header("Walkable Area")]
+        [SerializeField] private Vector2 minAreaPoint;
+        [SerializeField] private Vector2 maxAreaPoint;
         private string firstName;
         private string lastName;
         public NpcTypeEnum NpcType { get; private set; }
         public int IndoctrinationValue { get; private set; }
         public string DisplayName => $"{firstName} {lastName}";
+
+        private void Start() {
+            StartCoroutine(WalkAround());
+        }
 
         public void GenerateVisitor(string firstName, string lastName,
             Sprite bodyAsset, Sprite headgearAsset)
@@ -30,6 +39,35 @@ namespace Horticultist.Scripts.Mechanics
             headgearSprite.sprite = headgearAsset;
             NpcType = NpcTypeEnum.Visitor;
             IndoctrinationValue = 0;
+        }
+
+        private IEnumerator WalkAround()
+        {
+            var isIdle = Random.Range(0f, 1f) < 0.5f;
+            while(isIdle)
+            {
+                yield return new WaitForSeconds(1f);
+                isIdle = Random.Range(0f, 1f) < 0.5f;
+            }
+
+            var destination = GetRandomDestination();
+            var tween = transform.DOMove(destination, 0.5f)
+                .SetSpeedBased(true)
+                .SetEase(Ease.Linear)
+                .OnComplete(() => {
+                    StopCoroutine(WalkAround());
+                    StartCoroutine(WalkAround());
+                })
+                .SetId("npc");
+        }
+
+        
+        private Vector2 GetRandomDestination()
+        {
+            var posX = Random.Range(minAreaPoint.x, maxAreaPoint.x);
+            var posY = Random.Range(minAreaPoint.y, maxAreaPoint.y);
+
+            return new Vector2(posX, posY);
         }
     }
 }
--- Assets/Scripts/Mechanics/PlazaNpcInfoPanel.cs
+++ Assets/Scripts/Mechanics/PlazaNpcInfoPanel.cs
@@ -4,6 +4,7 @@ namespace Horticultist.Scripts.Mechanics
     using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.SceneManagement;
+    using DG.Tweening;
 
     [RequireComponent(typeof(NpcController))]
     public class PlazaNpcInfoPanel : MonoBehaviour
@@ -11,6 +12,7 @@ namespace Horticultist.Scripts.Mechanics
         public void StartTherapy()
         {
             GameStateController.Instance.SetSelectedNpc(GetComponent<NpcController>());
+            DOTween.Pause("npc");
             StartCoroutine(LoadTherapyScene());
         }
 
